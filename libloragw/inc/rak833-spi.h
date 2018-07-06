0d18a1eb08a97788c4a8712ff4f8da0870d80621
Jac Kersing <j.kersing@the-box.com>
2018-07-06 19:20:47 +0200

    Add definitions for RAK833 board

---
+++ libloragw/inc/rak833-spi.h
@@ -0,0 +1,19 @@
+/*
+ * rak833-spi
+ *
+ *  Created on: Jul 2, 2018
+ *      Author: kersing
+ */
+
+#ifndef _RAK833_SPI_H_
+#define _RAK833_SPI_H_
+
+/* Human readable platform definition */
+#define DISPLAY_PLATFORM "RAK634 + RAK833"
+
+/* parameters for native spi */
+#define SPI_SPEED		8000000
+#define SPI_DEV_PATH	"/dev/spidev32766.1"
+#define SPI_CS_CHANGE   0
+
+#endif /* _RAK833_SPI_H_ */
